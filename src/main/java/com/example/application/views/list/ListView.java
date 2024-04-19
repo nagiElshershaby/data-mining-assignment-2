4613e81ca478387008b229c7675acea919b69218
Nagi Mohamed <117310405+nagiElshershaby@users.noreply.github.com>
2024-04-19 16:25:56 +0200

    added UI input fields and buttons

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,33 +1,86 @@
 package com.example.application.views.list;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.example.application.data.Movie;
+import com.example.application.services.Kmeans;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.upload.Upload;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 
 @PageTitle("list")
 @Route(value = "")
 public class ListView extends VerticalLayout {
 
     public ListView() {
-        setSpacing(false);
+        Kmeans kmeans = new Kmeans();
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        IntegerField percentageField = new IntegerField("Percentage of data to read");
+        IntegerField clustersField = new IntegerField("Number of clusters (K)");
+        IntegerField iterationsField = new IntegerField("Number of iterations");
+        NumberField outlierField = new NumberField("outliers distance");
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        Div Suffix = new Div();
+        Suffix.setText("%");
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        percentageField.setSuffixComponent(Suffix);
+        percentageField.setMin(0);
+        percentageField.setMax(100);
+        percentageField.setValue(100);
+        clustersField.setValue(5);
+        iterationsField.setValue(100);
+        outlierField.setValue(1.0);
+
+        MultiFileMemoryBuffer buffer = new MultiFileMemoryBuffer();
+        Upload upload = new Upload(buffer);
+
+
+        upload.addSucceededListener(event -> {
+            String fileName = event.getFileName();
+            InputStream inputStream = buffer.getInputStream(fileName);
+
+            Notification.show("File uploaded successfully!");
+            kmeans.readMoviesFromFile(inputStream, percentageField.getValue());
+        });
+
+        Button processButton = new Button("Process");
+        processButton.addClickListener(event -> {
+            // validate input
+            if (percentageField.isEmpty() || clustersField.isEmpty() || iterationsField.isEmpty() || outlierField.isEmpty()) {
+                Notification.show("Please fill all the fields");
+                return;
+            }
+            if (percentageField.getValue() <= 1 || percentageField.getValue() > 100) {
+                Notification.show("Please enter a valid percentage value");
+                return;
+            }
+
+
+            Notification.show("Processing...");
+            kmeans.K_means_algorithm(clustersField.getValue(), iterationsField.getValue(), outlierField.getValue());
+            Notification.show("Processed!");
+        });
+
+        HorizontalLayout NumericInput = new HorizontalLayout();
+        HorizontalLayout Buttons = new HorizontalLayout();
+        NumericInput.add(percentageField, clustersField, iterationsField, outlierField);
+        Buttons.add(upload, processButton);
+        NumericInput.setAlignItems(Alignment.BASELINE);
+        Buttons.setAlignItems(Alignment.BASELINE);
+
+        add(NumericInput, Buttons);
     }
 
 }
